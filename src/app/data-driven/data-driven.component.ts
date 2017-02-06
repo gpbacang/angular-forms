65492f47976a258825807a8fc2949a26a7719493
Geraldine <geraldine@g-angle.co.jp>
2017-02-06 17:24:49 +0800

    Add validators

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'data-driven',
@@ -10,9 +10,11 @@ export class DataDrivenComponent {
 
   constructor() {
     this.myForm = new FormGroup({
-      'username': new FormControl(),
-      'email': new FormControl(),
-      'password': new FormControl(),
+      'username': new FormControl('Max', Validators.required),
+      'email': new FormControl('', [
+        Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+       ]),
+      'password': new FormControl('', Validators.required)
     });
   }
 
